02d865556354d2ba70a0dc710e2764842726e785
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-28 21:25:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ function placeOrder(cardNumber)
   {
     return `Sorry, we don't have a credit card on file for you.`
   }
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}. s`
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
