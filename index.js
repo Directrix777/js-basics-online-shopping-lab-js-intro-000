d6248173f24aaa338ab4ff1ef8a618861185eefe
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-28 20:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function setCart(c)
 function addToCart(item)
 {
  cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100 )})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart()
