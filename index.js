e9ef940ee98ad3cdbc57295c658bb4be0204cdbe
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-28 21:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,9 +62,5 @@ function removeFromCart(item)
 
 function placeOrder(cardNumber)
 {
-  var numDetect = [cardNumber]
-  if(numDetect.length = 1)
-  {
-    return `Sorry, we don't have a credit card on file for you.`
-  }
+  
 }
