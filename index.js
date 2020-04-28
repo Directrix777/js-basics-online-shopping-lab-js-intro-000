66e699c79897756d83eb96c0fa13163682ae307e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-28 20:22:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ function addToCart(item)
 
 function viewCart()
 {
-  // write your code here
+  if(cart.length < 1)
+  {
+    return 'Your shopping cart is empty.'
+  }
+  string = `In your cart, you have `
+  if(cart.length = 1)
+  {
+    return `${string + cart[0].itemName} at ${cart[0].itemPrice}.`
+  }
+  for(let i = 0; i < cart.length)
 }
 
 function total()
