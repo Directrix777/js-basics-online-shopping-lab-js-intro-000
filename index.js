2204f50664e4afc33cddf26b3df2a926cdbfe013
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-28 21:27:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,9 +62,11 @@ function removeFromCart(item)
 
 function placeOrder(cardNumber)
 {
-  if(cardNumber == undefined)
+  if(cardNumber === undefined)
   {
     return `Sorry, we don't have a credit card on file for you.`
   }
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
+  return string
 }
